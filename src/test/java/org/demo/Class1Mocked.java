5aa9df3d4ffab02339081d1cf1a8fe691d1be20a
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-07-02 13:39:51 +0800

    增加 demo 示例

---
+++ src/test/java/org/demo/Class1Mocked.java
@@ -0,0 +1,19 @@
+package org.demo;
+
+/**
+ * Created by 64274 on 2019/7/2.
+ *
+ * @ Description: TODO
+ * @ author  山羊来了
+ * @ date 2019/7/2---13:26
+ */
+public class Class1Mocked {
+
+    public  String hello(String name){
+        System.out.println("hello "+name);
+        return "hello "+name;
+    }
+    public void show(){
+        System.out.println("Class1Mocked.show()");
+    }
+}
